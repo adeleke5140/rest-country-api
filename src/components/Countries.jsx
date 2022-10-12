d271bc2e406f4fc4cf116c9c91569b2c0f669883
Dacron <favouradebimpe63@gmail.com>
2022-10-12 14:36:14 +0100

    added transformedData

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -18,21 +18,70 @@ const imageVariant ={
 function Countries({theme, searchCountry,query,SetQuery}) {
    const [isloading, setisLoading] = useState(true)
     
-    
+  
 
     
+ useEffect(() => {
+  axios.get(`https://restcountries.com/v3.1/all`)
+    .then(res => {
+      // console.log(res.data)
+      // Filtered only the required data from the request
+      const transformedData = res.data.map(country => {
+        return {
+          name: country?.name?.common,
+          population: country?.population,
+          subregion: country?.subregion,
+          region: country?.region,
+          capital: country?.capital?.join(", "),
+          tld: country?.tld?.join(", "),
+          flags:  country?.flags?.png,
+          get currencies() {
+            let result = [];
+            for (let key in country?.currencies) {
+              result.push(country?.currencies[key].name)
+            }
+            return result.join(", ")
+          },
+          get languages() {
+            let result = [];
+            for (let key in country?.languages) {
+              result.push(country?.languages[key])
+            }
+            return result.join(", ")
+          },
+          get nativeName(){
+            let result = [];
+            for (let key in country?.name?.nativeName) {
+              result.push(country?.name?.nativeName[key].common)
+            }
+            return result.join(", ")
+          },
+          get borders() {
+            let result = [];
+            country.borders?.forEach(item => {
+              res.data?.forEach(x => {
+                if (x.cca3 === item) {
+                  result.push(x.name.common)
+                }
+              })
+            })
+            if (!result || result.length === 0) {
+              return `No border country`;
+            }
+            return result.join(", ");
+          }
+        }
+      })
+      console.log(transformedData);
 
-    useEffect(()=>{
-     axios.get(`https://restcountries.com/v3.1/all`)
-     .then(res => {
-      console.log(res.data)
-      SetQuery(res.data)
+      SetQuery(transformedData)
       setisLoading(false)
-     })
-     .catch(err =>{
+    })
+    .catch(err => {
       console.log(err)
-     })
-    },[])
+    })
+}, [])
+
 
     if (isloading) {
       return <motion.img variants={imageVariant} animate='loading' src={loading} className='loading-img' alt="loadingImg" />
@@ -52,20 +101,22 @@ function Countries({theme, searchCountry,query,SetQuery}) {
           if (searchCountry == ''){
             return data
           }
-          else if(data?.name?.common.toLowerCase().includes(searchCountry.toLowerCase())){
+          else if(data.name.toLowerCase().includes(searchCountry.toLowerCase())){
             return data
           }
         }).map((data, index) =>  
-      <Link to={`/info/${data?.name?.common}/${data.population}/${data.region}/${data.capital}/${data.subregion}`}>
+      <Link to={`/info/${data.name}/${data.population}/${data.region}/${data.capital}/${data.subregion}/${data.languages}/${data.nativeName}/${data.currencies}/${data.borders}`}>
        <article key={index}>
           <div >
-          <img src={data?.flags?.png} alt={data?.name?.official}  />
+          <img src={data.flags} alt={data.name}  />
         
       <div id="details"  className={theme ? 'dark-mode' : 'light-mode' }>
-           <h3> {data?.name?.common} </h3>
+           <h3> {data.name} </h3>
           <p> Population: {data.population} </p>
           <p> Region: {data.region} </p>
           <p> Capital: {data.capital} </p>
+          <p> NativeName: {data.borders} </p>
+
       </div>
        
         </div>
